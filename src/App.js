83786644102933ed2e8383d94cedac21f5f629e4
AdamTronchin <atdraomnchin4@gmail.com>
2020-07-17 17:11:39 -0400

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Auth from '../src/auth/Auth'
+import './App.css';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+  state = {
+    info: [
+      {
+        firstName: "Adam",
+        userId: 1872
+      },
+      {
+        firstName: "Joe",
+        userId: 1976
+      }
+    ]
+  }
+
+  switchNameHandler = (name) =>{
+    this.setState({
+      info: [
+        {
+          firstName: name,
+          userId: 1872
+        },
+        {
+          firstName: "Joe",
+          userId: 1976
+        }
+      ]
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <button onClick={() => this.switchNameHandler("Max")}>Change</button>
+        <Auth firstName={this.state.info[1].firstName}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/auth/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Auth(info) {
+    return (
+        <div>
+            <p>{info.firstName}</p>
+        </div>
+    );
+}
+
+export default Auth;
