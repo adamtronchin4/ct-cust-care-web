2f3134d685f5e0da73d156b3b6f0ad177c611e90
AdamTronchin <atdraomnchin4@gmail.com>
2020-07-17 23:48:16 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
 import Auth from '../src/auth/Auth'
 import './App.css';
+// import logo1 from './logo1.png';
+// import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+
 
 class App extends Component {
 
@@ -35,8 +38,30 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <button onClick={() => this.switchNameHandler("Max")}>Change</button>
+        {/* <Navbar bg="light" expand="lg">
+          <img src={logo1} className="" alt="logo" height="40px" width="40px"/>
+          <Navbar.Brand>CT Customer Care Portal</Navbar.Brand>
+          <Navbar.Toggle aria-controls="CT Customer Care" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#">Call Back</Nav.Link>
+                <Nav.Link href="#">Rain Checks</Nav.Link>
+                <Nav.Link href="#">To-Do Lists</Nav.Link>
+                <Nav.Link href="#">My Notes</Nav.Link>
+                <Nav.Link href="#">Group Notes</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar> */}
+  
+        <br/>
+        <button className="btn btn-primary" onClick={() => this.switchNameHandler("Max")}>Change</button>
+
         <Auth firstName={this.state.info[1].firstName}/>
+
       </div>
     );
   }
--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Auth(info) {
     return (
         <div>
-            <p>{info.firstName}</p>
+            <p >{info.firstName}</p>
         </div>
     );
 }
